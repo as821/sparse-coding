adb2590054bd369f4542643ecf51e7d2217a796b
as821 <andrew.r.stange@gmail.com>
2024-08-24 21:31:59 -0700

    works on linux now

--- src/c/build.sh
+++ src/c/build.sh
@@ -5,7 +5,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
 elif [[ "$(uname)" == "Linux" ]]; then
     # Linux
     OPENBLAS_INCLUDE="/usr/include/x86_64-linux-gnu/openblas-pthread"
-    OPENBLAS_LIB="/usr/lib/x86_64-linux-gnu"
+    OPENBLAS_LIB="/usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.a"
 else
     echo "Unsupported operating system"
     exit 1
@@ -16,7 +16,7 @@ OPTIM_OPTIONS="-O3 -fno-math-errno -fno-trapping-math -march=native"
 GCC_OPTIONS="-c -g -I./src/c/src $OPTIM_OPTIONS -o"
 
 gcc -fPIC -I"$OPENBLAS_INCLUDE" $GCC_OPTIONS src/c/bin/fista.o src/c/src/fista.c
-gcc -shared -g -lopenblas -L"$OPENBLAS_LIB" $OPTIM_OPTIONS -o src/c/bin/fista.so src/c/bin/fista.o
+gcc -shared -g -L"$OPENBLAS_LIB" $OPTIM_OPTIONS -o src/c/bin/fista.so src/c/bin/fista.o -lopenblas
 
 rm -f src/c/bin/*.o
 rm -rf src/c/bin/*.dSYM
