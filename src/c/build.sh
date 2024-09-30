6702e63984132454493003ac30cba8f1eedef0dd
Your Name <you@example.com>
2024-09-29 21:56:33 -0700

    comment

--- src/c/build.sh
+++ src/c/build.sh
@@ -15,8 +15,8 @@ fi
 OPTIM_OPTIONS="-O3 -fno-math-errno -fno-trapping-math -march=native"
 GCC_OPTIONS="-c -g -I./src/c/src $OPTIM_OPTIONS -o"
 
-gcc -fPIC -fopenmp -I"$OPENBLAS_INCLUDE" $GCC_OPTIONS src/c/bin/fista.o src/c/src/fista.c
-gcc -shared -g -L"$OPENBLAS_LIB" $OPTIM_OPTIONS -o src/c/bin/fista.so src/c/bin/fista.o -lopenblas
+# gcc -fPIC -fopenmp -I"$OPENBLAS_INCLUDE" $GCC_OPTIONS src/c/bin/fista.o src/c/src/fista.c
+# gcc -shared -g -L"$OPENBLAS_LIB" $OPTIM_OPTIONS -o src/c/bin/fista.so src/c/bin/fista.o -lopenblas
 
 ADA_OPTIONS="-O3 -arch=sm_89 -gencode=arch=compute_89,code=sm_89 -gencode=arch=compute_89,code=compute_89"
 nvcc -shared -g -lcublas -lcuda -Xptxas="-v" -Xptxas="-dlcm=cg" -lineinfo -std=c++17 $ADA_OPTIONS -o src/c/bin/cu_fista_89.so src/c/src/fista.cu -Xcompiler="-fPIC $OPTIM_OPTIONS" -diag-suppress 2464
